3c10e521fd00ba2629dfd6618a43664e29bd72df
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-12-26 21:19:13 +0900

    get conference

--- src/scrap-paper/main.ts
+++ src/scrap-paper/main.ts
@@ -1,3 +1,7 @@
+const addLink = (text: string) => {
+  return "[" + text + "]";
+};
+
 const title_collection = document.getElementsByClassName(
   "citation__title"
 ) as HTMLCollectionOf<HTMLElement>;
@@ -7,14 +11,26 @@ const title = title_collection[0].innerText;
 const author_collection = document.getElementsByClassName(
   "author-name"
 ) as HTMLCollectionOf<HTMLElement>;
-const addLink = (text: string) => {
-  return "[" + text + "]";
-};
+
 const authors = Array.from(author_collection).map((e) => {
   return addLink(e.title);
 });
 
-const lines: string = authors.join(", ");
+const item_detail_collection = document.getElementsByClassName(
+  "issue-item__detail"
+) as HTMLCollectionOf<HTMLElement>;
+const conference_with_year = ((): string => {
+  const e = item_detail_collection[0].children[0] as HTMLElement;
+  return e.title.split(":")[0];
+})();
+const conference: string = conference_with_year.split(" '")[0];
+console.log(conference_with_year);
+const lines: string =
+  addLink(conference) +
+  " " +
+  addLink(conference_with_year) +
+  "\n" +
+  authors.join(", ");
 const proj: string = "tomoki52";
 var body = encodeURIComponent(lines);
 window.open(
