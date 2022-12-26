a95e27422270a348041f03c8bbc80f79c2e17da2
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-12-26 14:25:25 +0900

    get title and author

--- src/scrap-paper/main.ts
+++ src/scrap-paper/main.ts
@@ -1,5 +1,20 @@
-const title: string = document.title;
-const lines: string = "テスト";
+const title_collection = document.getElementsByClassName(
+  "citation__title"
+) as HTMLCollectionOf<HTMLElement>;
+
+const title = title_collection[0].innerText;
+
+const author_collection = document.getElementsByClassName(
+  "author-name"
+) as HTMLCollectionOf<HTMLElement>;
+const addLink = (text: string) => {
+  return "[" + text + "]";
+};
+const authors = Array.from(author_collection).map((e) => {
+  return addLink(e.title);
+});
+
+const lines: string = authors.join(", ");
 const proj: string = "tomoki52";
 var body = encodeURIComponent(lines);
 window.open(
